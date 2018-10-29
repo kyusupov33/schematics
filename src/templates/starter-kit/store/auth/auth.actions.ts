c9dd9b9bcc97c05680fea44dbfa8a47de5c8babe
Max Ivanov <omaxphp@yandex.ru>
2018-10-29 16:27:03 +0300

    feat: add schematics for @ngxs/store (#3)
    * feat: add @ngxs/schematics
    
    * fix: library name updated
    
    * fix: resolve review
    
    * Update README.md
    
    * fix: change to select snapshot

---
+++ src/templates/starter-kit/store/auth/auth.actions.ts
@@ -0,0 +1,8 @@
+import { AuthenticationStateModel } from './auth.state';
+
+export class SetAuthData {
+    public static readonly type = '[Auth] Auth data';
+
+    constructor(public payload: AuthenticationStateModel) {
+    }
+}
--- src/templates/store/__name__.state.spec.ts
+++ src/templates/store/__name__.state.spec.ts
@@ -1,9 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
 import { NgxsModule, Store } from '@ngxs/store';
-import { {{pascalCase name}}State } from './<%= dasherize(name) %>.state';
-import { {{pascalCase name}}Action } from './<%= dasherize(name) %>.actions';
+import { <%= classify(name) %>State, <%= classify(name) %>StateModel } from './<%= dasherize(name) %>.state';
+import { <%= classify(name) %>Action } from './<%= dasherize(name) %>.actions';
 
-describe('<%= classify(name) %> actions', () => {
+describe('<%= classify(name) %> store', () => {
   let store: Store;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -13,10 +13,12 @@ describe('<%= classify(name) %> actions', () => {
   }));
 
   it('should create an action and add an item', () => {
+    const expected: <%= classify(name) %>StateModel = {
+      items: ['item-1']
+    };
     store.dispatch(new <%= classify(name) %>Action('item-1'));
-    store.select(state => state.<%= classify(name) %>.items).subscribe((items: string[]) => {
-      expect(items).toEqual(jasmine.objectContaining([ 'item-1' ]));
-    });
+    const actual = store.selectSnapshot(<%= classify(name) %>State.getState);
+    expect(actual).toEqual(expected);
   });
 
 });
